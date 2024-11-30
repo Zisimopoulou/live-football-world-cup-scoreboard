797cbe4087d8bfacf6720d69202e3903ec0006a1
Zisimopoulou <zisimopouloua@protonmail.com>
2024-11-30 17:22:55 +0200

    Add test to verify sorting of matches in progress by total score when the total scores are different.

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -99,4 +99,29 @@ public class ScoreboardTest {
             scoreboard.finishMatch("Mexico", "Canada");
         });
     }
+
+    @Test
+    void getMatchesInProgress_GivenDifferentScores_ShouldSortByTotalScore() {
+        scoreboard.startMatch("Mexico", "Canada");
+        scoreboard.updateScore("Mexico", "Canada", 0, 5);
+
+        scoreboard.startMatch("Spain", "Brazil");
+        scoreboard.updateScore("Spain", "Brazil", 10, 2);
+
+        scoreboard.startMatch("Germany", "France");
+        scoreboard.updateScore("Germany", "France", 2, 2);
+
+        scoreboard.startMatch("Uruguay", "Italy");
+        scoreboard.updateScore("Uruguay", "Italy", 6, 6);
+
+        scoreboard.startMatch("Argentina", "Australia");
+        scoreboard.updateScore("Argentina", "Australia", 3, 1);
+
+        List<Match> matches = scoreboard.getSortedMatchesInProgress();
+
+        assertEquals("Spain 10 - 2 Brazil", matches.get(0).toString(), "Spain 10 - 2 Brazil should be listed first");
+        assertEquals("Mexico 0 - 5 Canada", matches.get(1).toString(), "Mexico 0 - 5 Canada should be listed second");
+        assertEquals("Argentina 3 - 1 Australia", matches.get(2).toString(), "Argentina 3 - 1 Australia should be listed last");
+    }
+
 }
